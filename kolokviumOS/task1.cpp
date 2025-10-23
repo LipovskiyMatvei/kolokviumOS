ac63fbe1a52d6b1a8c5b27b94af0c8972e3d639c
lipov <lipov@LAPTOP-OIV4E4VL>
2025-10-23 12:37:49 +0300

    Добавьте файлы проекта.

---
+++ kolokviumOS/task1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <limits>
+
+using namespace std;
+
+
+bool fib(long long n, long long& result) {
+    if (n == 0) { result = 0; return true; }
+    if (n == 1) { result = 1; return true; }
+    if (n > 92) return false; 
+
+    long long a = 0, b = 1;
+    for (long long i = 2; i <= n; ++i) {
+        long long c = a + b;
+        a = b;
+        b = c;
+    }
+    result = b;
+    return true;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "Enter n to count Fibonacci: ";
+
+    long long n;
+    if (!(cin >> n)) {
+        cerr << "Input error: expected integer.\n";
+        return 1;
+    }
+    if (n < 0) {
+        cerr << "Invalid input: n must be > 0"<< endl;
+        return 1;
+    }
+
+    long long value;
+    if (fib(n, value)) {
+        cout << value << '\n';
+        return 0;
+    }
+
+   
+    cerr << "Overflow: exact Fibonacci for n = " << n;
+        
+    return 1;
+}
---
+++ task2/task2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cout << "Enter string: ";
+    getline(cin, s);
+
+    string original = s;
+    reverse(s.begin(), s.end());
+
+    cout << (original == s ? "Palindrom!" : "Not palindrom.") << endl;
+
+    return 0;
+}
